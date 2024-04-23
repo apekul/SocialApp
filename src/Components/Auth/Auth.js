09ef279883a71facdd5b7ed24d4ce8135e3103bd
Adaś <adampekul@gmail.com>
2024-04-23 18:51:32 +0200

    add login and register components from MUI

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Register from "./Register";
+import Login from "./Login";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [showLogin, setShowLogin] = useState(false);
+  return (
+    <div className=" flex items-center h-[100vh]">
+      {showLogin ? (
+        <Login setShowLogin={setShowLogin} />
+      ) : (
+        <Register setShowLogin={setShowLogin} />
+      )}
+    </div>
+  );
 };
 
 export default Auth;
